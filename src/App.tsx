124bc870f4a5ae82201f9ea533cc7de643b78b97
Rich Zhao <zhao.zhenhua@gmail.com>
2022-11-05 18:34:04 +0800

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from "react";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Service from "./components/Services";
 
 function App() {
-
-  useEffect(() => {
-  document.title = "Bytehub+"; }, [])
-
   return (
     <div>
       <Navbar />
