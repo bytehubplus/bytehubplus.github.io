cb01cf630f08f9752e3f6776ddcd4f99de400ef1
Rich Zhao <zhao.zhenhua@gmail.com>
2022-11-05 16:16:12 +0800

    draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Service from "./components/Services";
 
 function App() {
+
+  useEffect(() => {
+  document.title = "Bytehub+"; }, [])
+
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Service />
+      <Footer />
     </div>
   );
 }
