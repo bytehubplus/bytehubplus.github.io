2ee2d04f7dcc1120df4d0482757898668d2dff64
Rich Zhao <zhao.zhenhua@gmail.com>
2023-01-28 09:09:15 +0800

    remove docusaurus generated files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/rich/repos/bytehubplus/bytehubplus.github.io/node_modules/infima/dist/css/default/default.css'),
-  require('/home/rich/repos/bytehubplus/bytehubplus.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/rich/repos/bytehubplus/bytehubplus.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/rich/repos/bytehubplus/bytehubplus.github.io/src/css/custom.css'),
-];
